57be076d23141759f3af0526f90c9f51bf244ac4
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-07-23 10:16:49 +0900

    feat:Add cat class

---
+++ src/CatNew.java
@@ -0,0 +1,10 @@
+public class CatNew {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        cat.setName("Taroooooo");
+        cat.SetAge(4);
+        cat.showProfile();
+        cat.showMyFriends();
+    }
+}
