e2f4ffc0cf16ebe203d93d6d2d64646044cf8ac9
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-03 08:29:17 +0900

    feat:Add stream API

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,22 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
-
     public static void main(String[] args) {
-        // 第3回
-        // List
-        List<String> alcoholArray = new ArrayList<>();
-        alcoholArray.add("ビール");
-        alcoholArray.add("ワイン");
-        alcoholArray.add("日本酒");
-        alcoholArray.add("焼酎");
-        alcoholArray.add("酎ハイ");
-        alcoholArray.add("カクテル");
-        System.out.println(alcoholArray.toString());
-        for (String alcohol : alcoholArray) {
-            System.out.println(alcohol);
-        }
+        List<String> areaList = Arrays.asList(
+                "Tokyo",
+                "Chiba",
+                "Saitama",
+                "Kanagawa",
+                "Gunma",
+                "Tochigi",
+                "Ibaraki",
+                "Yamanashi"
+        );
+        Stream<String> stream = areaList.stream();
 
-        // 例外処理
-        String userAge = "hogehoge";
-        try {
-            int age = Integer.parseInt(userAge);
-            if (age >= 20) {
-                System.out.println("You can get Alcohol!!");
-            } else {
-                System.out.println("You are too young to get Alcohol!!");
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("INPUT DATA IS NOT NUMBER!!");
-            throw e;
-        }
-
+        Stream<String> stream2 = stream.filter(str -> str.length() >= 6);
+        stream2.forEach(str -> System.out.println(str));
     }
 }
