ed7f0bb8fec7b400d5b4d8240928492833726f56
takahiro-okada <76928095+takahiro-okada@users.noreply.github.com>
2022-08-05 09:16:00 +0900

    feat:Add stream

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        //        6文字以上の関東エリアの県名を表示する
         List<String> areaList = Arrays.asList(
                 "Tokyo",
                 "Chiba",
@@ -15,8 +16,18 @@ public class Main {
                 "Yamanashi"
         );
         Stream<String> stream = areaList.stream();
-
         Stream<String> stream2 = stream.filter(str -> str.length() >= 6);
         stream2.forEach(str -> System.out.println(str));
+
+        //        Streamを連結する書き方
+        List<String> fruits = Arrays.asList("Orange", "Apple", "Lemon", "Grape", "Strawberry");
+        List<String> myFavoriteFruits = Arrays.asList("Apple", "Lemon");
+        fruits.stream()
+                //                フルーツをループ処理で表示する
+                .forEach(v -> {
+                    System.out.println(v);
+                });
+        
+
     }
 }
